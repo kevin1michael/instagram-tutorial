2cccb31e6ec8a993b04bd25039653e2244bc749d
Kevin Michael <kevin.michael.905@gmail.com>
2023-11-02 14:17:57 -0400

    upgraded tempo dev tools

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    swcPlugins: process.env.NEXT_PUBLIC_TEMPO
+      ? [[require.resolve("@petergok/tempo-devtools/swc")], {}]
+      : [],
+  },
   images: {
     unoptimized: true,
   },
